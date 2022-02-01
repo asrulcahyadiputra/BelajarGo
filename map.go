9ad4ee5f2b2e4a5252196c9b7c545aea0253a662
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-01 20:09:52 +0800

    map data type

---
+++ map.go
@@ -0,0 +1,28 @@
+package main 
+
+import "fmt"
+
+func main(){
+    person := map[string]string{
+        "name" : "Asrul",
+        "address": "Makassar",
+    }
+
+    person["title"] = "Backend Developer"
+    fmt.Println(person)
+    fmt.Println(person["name"])
+    fmt.Println(person["address"])
+    fmt.Println(person["title"])
+
+    book := make(map[string]string)
+
+    book["title"] = "Belajar Golang Dasar"
+    book["author"] = "Asrul"
+    book["ups"] = "Salah"
+
+    fmt.Println(book)
+    fmt.Println(len(book))
+    delete(book,"ups")
+    fmt.Println(book)
+    fmt.Println(len(book))
+}
