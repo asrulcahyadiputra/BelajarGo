6351d5c24cf7cc320215ab6a5c05fbfb43bc51ef
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-01 18:49:51 +0800

    comparation

---
+++ comparation.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+    // opertator perbandingan
+    var a = 10 
+    var b = 5
+
+    var result bool = a == b 
+    var result1 bool = a > b 
+    var result2 bool = a < b 
+    var result3 bool = a >= b 
+    var result4 bool = a <= b 
+
+    fmt.Println(result)
+    fmt.Println(result1)
+    fmt.Println(result2)
+    fmt.Println(result3)
+    fmt.Println(result4)
+}
