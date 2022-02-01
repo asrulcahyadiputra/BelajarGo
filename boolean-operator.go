8a49f2c62103c562153171b5870aef63c512c07a
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-01 19:05:26 +0800

    boolean operator

---
+++ boolean-operator.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main(){
+    // Operator boolean
+    var ujian = 80
+    var absensi = 60
+
+    var lulusUjian = ujian >= 80
+    var lulusAbsensi = absensi >= 75
+
+    var lulus bool = lulusUjian && lulusAbsensi 
+
+    fmt.Println(lulusUjian)
+    fmt.Println(lulusAbsensi)
+    fmt.Println(lulus)
+}
