8e5b334b843e1dd83f4581c029a0a47b5a9aa8ab
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-01 18:20:24 +0800

    variable

---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+
+	name = "Asrul Cahyadi Putra"
+	fmt.Println(name)
+
+	name = "Cahyadi Putra"
+	fmt.Println(name)
+
+	var age = 23
+	fmt.Println(age)
+
+	// ini variable
+	thisVariable := "Asrul Cahyadi Putra"
+	fmt.Println(thisVariable)
+
+	var (
+		firstName = "Asrul Cahyadi"
+		lastName = "Putra"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
